226213edfd222e13b68b069b42aa4be44b187f6b
Dantae AKA Dogecraft <dantaealt@gmail.com>
2026-07-20 22:01:36 -0700

    fix: update discord rpc to show dev mode

--- src/main/rpc.ts
+++ src/main/rpc.ts
@@ -1,5 +1,6 @@
 import { ipcMain } from "electron"
 import { Client, ActivityType, PresenceBuilder } from "discord-rpc-new"
+import { is } from "@electron-toolkit/utils"
 import jsonData from "../../package.json"
 const clientId = "1403970186956247052"
 let rpcClient: Client | null = null
@@ -10,8 +11,7 @@ function startDiscordRPC(): boolean {
 
     const activity = new PresenceBuilder()
       .setType(ActivityType.Playing)
-      .setDetails("Aiming with precision")
-      .setState(`Using Dotline v${jsonData.version || "0"}`)
+      .setState(`Using Dotline v${jsonData.version || "0"}${is.dev ? " (dev)" : ""}`)
       .setLargeImage("dotline2", "Dotline Crosshair Overlay")
       .addButton("Download Dotline", "https://parcoil.com/dotline")
       .addButton("Join Discord", "https://discord.com/invite/En5YJYWj3Z")
